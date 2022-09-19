26d3cedef192f41e08d8490f5f3ed3a9fecabee9
neum <elettrochuck@gmail.com>
2022-09-19 23:09:36 +0200

    update

--- ESP32/pushes/pushes.ino
+++ ESP32/pushes/pushes.ino
@@ -1,31 +1,21 @@
-/*
-  SilentStepStick TMC2208/TMC2209 Example
-  Rsense: 0.11 Ohm
-  Other examples/libraries can be found here:
-  https://github.com/teemuatlut/TMCStepper
-  https://github.com/trinamic/TMC-API
-  https://github.com/manoukianv/TMC2208Pilot
-  Example source code free to use.
-  Further information: https://learn.watterott.com/license/
-*/
-
 #include <Button2.h>
 #include <RotaryEncoder.h>
 
 
 // Note: You also have to connect GND, 5V/VIO and VM.
 //       A connection diagram can be found in the schematics.
-#define EN_PIN        26  //enable (CFG6)
-#define DIR_PIN       33  //direction
-#define STEP_PIN      25  //step
-#define ACTIVE        0
+#define EN_PIN1       26  //enable (CFG6)
+#define DIR_PIN1      33  //direction
+#define STEP_PIN1     25  //step
+#define ACTIVE1       0
+
 #define EN_PIN2       16  //enable (CFG6)
 #define DIR_PIN2      5  //direction
 #define STEP_PIN2     17  //step
 #define ACTIVE2       4
+
 #define BUTTONPIN1    27
 #define BUTTONPIN2    2
-
 #define ROTARY1_PIN1  14
 #define ROTARY1_PIN2  12
 #define ROTARY2_PIN1  15
@@ -34,15 +24,16 @@
 #define MAX_SPEED     5000
 #define MIN_SPEED     250
 #define DEF_SPEED     1000
+#define MULTER        100
 
-int debounce = 30;
+int debounce = 50;
 int button_state = 0;
 int button2_state = 0;
 
-Button2 button;
+Button2 button1;
 Button2 button2;
-RotaryEncoder encoder(ROTARY1_PIN1, ROTARY1_PIN2, RotaryEncoder::LatchMode::FOUR3);
-RotaryEncoder encoder1(ROTARY2_PIN1, ROTARY2_PIN2, RotaryEncoder::LatchMode::FOUR3);
+RotaryEncoder encoder1(ROTARY1_PIN1, ROTARY1_PIN2, RotaryEncoder::LatchMode::FOUR3);
+RotaryEncoder encoder2(ROTARY2_PIN1, ROTARY2_PIN2, RotaryEncoder::LatchMode::FOUR3);
 
 typedef struct {
   bool state;
@@ -52,7 +43,7 @@ typedef struct {
   int step_pin;
 } Motor;
 
-Motor motor1 = { false, 0, DEF_SPEED, LOW, STEP_PIN };
+Motor motor1 = { false, 0, DEF_SPEED, LOW, STEP_PIN1 };
 Motor motor2 = { false, 0, DEF_SPEED, LOW, STEP_PIN2 };
 
 void setup()
@@ -68,23 +59,24 @@ void setup()
   encoder1.setPosition(DEF_SPEED);
   
   
-  pinMode(ACTIVE, OUTPUT);
-  digitalWrite(ACTIVE, LOW);
-
+  pinMode(ACTIVE1, OUTPUT);
+  digitalWrite(ACTIVE1, LOW);
   pinMode(ACTIVE2, OUTPUT);
   digitalWrite(ACTIVE2, LOW);
   
   delay(2000);
   
-  pinMode(EN_PIN, OUTPUT);
-  digitalWrite(EN_PIN, HIGH); //deactivate driver (LOW active)
-  pinMode(DIR_PIN, OUTPUT);
-  digitalWrite(DIR_PIN, LOW); //LOW to CCW
-  //digitalWrite(DIR_PIN, HIGH); //HIGH to CW
-  pinMode(STEP_PIN, OUTPUT);
-  digitalWrite(STEP_PIN, LOW);
-  digitalWrite(EN_PIN, LOW); //activate driver
+  // motor 1
+  pinMode(EN_PIN1, OUTPUT);
+  digitalWrite(EN_PIN1, HIGH); //deactivate driver (LOW active)
+  pinMode(DIR_PIN1, OUTPUT);
+  digitalWrite(DIR_PIN1, LOW); //LOW to CCW
+  //digitalWrite(DIR_PIN1, HIGH); //HIGH to CW
+  pinMode(STEP_PIN1, OUTPUT);
+  digitalWrite(STEP_PIN1, LOW);
+  digitalWrite(EN_PIN1, LOW); //activate driver
 
+  // motor 2
   pinMode(EN_PIN2, OUTPUT);
   digitalWrite(EN_PIN2, HIGH); //deactivate driver (LOW active)
   pinMode(DIR_PIN2, OUTPUT);
@@ -99,59 +91,15 @@ void setup()
 void loop()
 {
   
-  button.loop();
-  //button2.loop();
-  //encoder.tick();
-  //encoder1.tick();
-  //check_speed(&motor1, encoder);
-  //check_speed(&motor2, encoder1);
-
-  static int pos = DEF_SPEED;
-  
-  encoder.tick();
-
-  int newPos = encoder.getPosition();
-  if (pos != newPos) {
-    Serial.print("pos:");
-    Serial.print(newPos);
-    Serial.print(" dir:");
-    Serial.println((int)(encoder.getDirection()));
-    int factor = newPos - pos;
-    int _newPos = constrain(factor * 100 + pos, MIN_SPEED, MAX_SPEED);
-    encoder.setPosition(_newPos);
-    pos = _newPos;
-    motor1.dur = _newPos;
-  } // if
-  
+  button1.loop();
+  button2.loop();
+  check_speed(&motor1, encoder1);
+  check_speed(&motor2, encoder2);
   check_motor(&motor1);
-  //check_motor(&motor2);
-
-  /*
-  if (button_state) {
-    digitalWrite(STEP_PIN, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(STEP_PIN, LOW);
-    delayMicroseconds(1000);
-  }
-
-  if (button2_state) {
-    digitalWrite(STEP_PIN2, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(STEP_PIN2, LOW);
-    delayMicroseconds(1000);
-  }
-  */
-  
+  check_motor(&motor2);
 }
 
-void released(Button2& btn) {
-  /*
-  Serial.print("RELEASED!\tBUTTON STATE:\t");
-  Serial.println(button_state);
-  Serial.print("ACTIVE2:\t");
-  Serial.println(digitalRead(ACTIVE2));
-  button_state = 1 - button_state; 
-  */
+void released(Button2 &btn) {
   if (btn == button) {
     motor1.state = !motor1.state;
   } else if (btn == button2) {
@@ -171,26 +119,14 @@ void check_motor(Motor *m) {
   }
 }
 
-void check_speed(Motor *m, RotaryEncoder _enc) { 
-  /*
-  int _dir = (int)(_enc.getDirection());
-  if (_dir != 0)
-    Serial.println(m->dur);
-  
-  m->dur = constrain(m->dur + _dir, MIN_SPEED, MAX_SPEED);
-  Serial.println(_dir);
-  */
+void check_speed(Motor *m, RotaryEncoder &_enc) { 
   _enc.tick();
-
   int newPos = _enc.getPosition();
-
   
   if (m->dur != newPos) {
-    Serial.print("pos:");
-    Serial.print(newPos);
-    Serial.print(" dir:");
-    Serial.println((int)(_enc.getDirection()));
-    m->dur = newPos;
+    int factor = newPos - m->dur;
+    int _newPos = constrain(factor * MULTER + m->dur, MIN_SPEED, MAX_SPEED);
+    _enc.setPosition(_newPos);
+    m->dur = _newPos;
   }
-  Serial.println(_enc.getPosition());
 }
